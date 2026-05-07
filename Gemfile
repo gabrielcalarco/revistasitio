be7614894b4fe6a6de1a428a6615091059d04308
Gabriel Calarco <gabcalarco@gmail.com>
2026-05-07 13:42:50 +0000

    cambios gemas

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source "https://rubygems.org"
 gemspec
-gem "jekyll", "~> 3.10.0"
 gem "base64"
 gem "bigdecimal"
 gem "mutex_m"
--- _site/assets/js/search.js
+++ _site/assets/js/search.js
@@ -2106,7 +2106,7 @@ function doSearch() {
   //Loop through, match, and add results
   for (var item in result) {
     var ref = result[item].ref;
-    var searchitem = '<div class="result"><p><a href="/viaje-al-rio-de-la-plata'+store[ref].link+'?q='+query+'">'+store[ref].title+'</a></p></div>';
+    var searchitem = '<div class="result"><p><a href="/revistasitio'+store[ref].link+'?q='+query+'">'+store[ref].title+'</a></p></div>';
     resultdiv.append(searchitem);
   }
 }
